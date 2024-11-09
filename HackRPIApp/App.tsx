2238f47134d72fc6661e1a71ca7e9cfad5eadf82
Adam <adam.smart830@gmail.com>
2024-11-09 16:07:46 -0500

    No clue!

--- HackRPIApp/App.tsx
+++ HackRPIApp/App.tsx
@@ -1,39 +1,20 @@
-import React from "react";
-import { Button, View, StyleSheet, SafeAreaView } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Amplify } from "aws-amplify";
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-
-import outputs from "./amplify_outputs.json";
-
-Amplify.configure(outputs);
-
-const SignOutButton = () => {
-  const { signOut } = useAuthenticator();
-
+export default function App() {
   return (
-    <View style={styles.signOutButton}>
-      <Button title="Sign Out" onPress={signOut} />
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
-
-const App = () => {
-  return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <SafeAreaView>
-          <SignOutButton />
-        </SafeAreaView>
-      </Authenticator>
-    </Authenticator.Provider>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  signOutButton: {
-    alignSelf: "flex-end",
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
